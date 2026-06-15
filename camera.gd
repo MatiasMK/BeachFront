a201dd95714011c5db0ff0cf81d4d0800ead762d
An <anuzky@live.com.ar>
2026-06-15 03:26:50 -0300

    New Hotel mesh, tree clusters and visual adjustments

--- camera.gd
+++ camera.gd
@@ -1,8 +1,12 @@
 extends Camera3D
 
 var curr_spot = 0 # current spot; in this case, left (0) or right (1)
-var positions = [Vector3(-8,9,12),Vector3(8,9,12)] # for each spot, a position
-var rotations = [Vector3(-0.5,-0.5,0),Vector3(-0.5,0.5,0)] # for each spot, an angle
+var positions = [Vector3(-5,15,11),Vector3(5,15,11)] # for each spot, a position
+var rotations = [Vector3(-0.9,-0.4,0),Vector3(-0.9,0.4,0)] # for each spot, an angle
+
+func _ready():
+	self.global_position = positions[0]
+	self.global_rotation = rotations[0]
 
 func switch_spot():
 	var tween = create_tween()
@@ -21,7 +25,8 @@ func switch_spot():
 	tween.tween_property(self, "global_rotation", rotations[curr_spot], duration) \
 		.set_trans(Tween.TRANS_SINE) \
 		.set_ease(Tween.EASE_OUT)
-
+	print(self.global_rotation)
+	print(self.rotation)
 func _input(event : InputEvent):
 	if event.is_action_pressed("move_cam"):
 		switch_spot()
